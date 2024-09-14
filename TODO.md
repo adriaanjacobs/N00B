5e636aaa6f2f711f9682ace403587d7078b50830
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2024-09-14 20:14:21 +0200

    update TODO & fix typo

--- TODO.md
+++ TODO.md
@@ -2,8 +2,9 @@
 
 ## Features
 - [ ] Re-introduce ASLR to the allocator
-- [ ] Support `realloc`
+- [x] Support `realloc`
 - [ ] Wrap & tag globals
 * Relocate and check stack allocations
     - [ ] Create stack allocator
     - [ ] Instrument stack allocations
+- [x] Support `calloc`
--- allocator/malloc_wrapper.cpp
+++ allocator/malloc_wrapper.cpp
@@ -32,7 +32,7 @@ void init_noob() {
     initialized = true;
 }
 
-#define IF_NOT_INSIDE_NOOB(expr)        \
+#define IF_INSIDE_NOOB(expr)        \
     if (inside_noob || !initialized)    \
         return expr;                    \
     set_inside_noob guard{};
@@ -41,19 +41,19 @@ extern "C" {
 
 decltype(malloc) __libc_malloc;
 void* malloc(size_t nbytes) {
-    IF_NOT_INSIDE_NOOB(__libc_malloc(nbytes));
+    IF_INSIDE_NOOB(__libc_malloc(nbytes));
     return noob_malloc(nbytes);
 }
 
 decltype(free) __libc_free;
 void free(void* ptr) {
-    IF_NOT_INSIDE_NOOB(__libc_free(ptr));
+    IF_INSIDE_NOOB(__libc_free(ptr));
     return noob_free(ptr);
 }
 
 decltype(realloc) __libc_realloc;
 void* realloc(void* oldptr, size_t newsize) {
-    IF_NOT_INSIDE_NOOB(__libc_realloc(oldptr, newsize));
+    IF_INSIDE_NOOB(__libc_realloc(oldptr, newsize));
     return noob_realloc(oldptr, newsize);
 }
 
@@ -63,7 +63,7 @@ void* reallocarray (void *ptr, size_t nmemb, size_t size) {
 
 decltype(memalign) __libc_memalign;
 void* memalign(size_t alignment, size_t size) {
-    IF_NOT_INSIDE_NOOB(__libc_memalign(alignment, size));
+    IF_INSIDE_NOOB(__libc_memalign(alignment, size));
     return noob_memalign(alignment, size);
 }
 
@@ -84,7 +84,7 @@ int posix_memalign (void **memptr, size_t alignment, size_t size) {
 
 decltype(calloc) __libc_calloc;
 void* calloc(size_t nmemb, size_t size) {
-    IF_NOT_INSIDE_NOOB(__libc_calloc(nmemb, size));
+    IF_INSIDE_NOOB(__libc_calloc(nmemb, size));
     return noob_calloc(nmemb * size);
 }
 
