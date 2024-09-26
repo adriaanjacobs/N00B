f1acb9ca60cf5fbbc334619dcf6a37c8a50695fa
Adriaan Jacobs <adriaan.hendrik.jacobs@huawei.com>
2024-09-26 00:35:49 +0000

    replace allocas with noobstack-allocated objs

--- llvm-plugin/llvm_noob.cpp
+++ llvm-plugin/llvm_noob.cpp
@@ -6,10 +6,14 @@
 #include <llvm-utils/safetyanalysis/safetyanalysis.h>
 
 #include <llvm/IR/Instructions.h>
+#include <llvm/IR/InstIterator.h>
+#include <llvm/Support/FormatVariadic.h>
+#include <llvm/Analysis/StackSafetyAnalysis.h>
 
 llvm::PreservedAnalyses NOOBInstrumentationPass::run(llvm::Module& module, llvm::ModuleAnalysisManager& MAM) {
+    // instrument all pointer dereferences to verify that the top bits match the in-pointer bits
+    // get analysis results _before_ we start modifying the module
     auto& unsafeAccessInfo = MAM.getResult<UnsafeAccessFinderAnalysis>(module).getOrCreate(false);
-
     auto int64Ty = llvm::Type::getInt64Ty(module.getContext());
     for (auto access : unsafeAccessInfo.unsafeAccesses) {
         auto insertBefore = access;
@@ -35,6 +39,75 @@ llvm::PreservedAnalyses NOOBInstrumentationPass::run(llvm::Module& module, llvm:
             auto store = llvm::cast<llvm::StoreInst>(access);
             ASSERT_ELSE_UNKOWN(store, access);
             store->setOperand(store->getPointerOperandIndex(), maskedPtr);
+        }
+    }
+
+    // now move all unsafe stack objects to per-size noobstacks
+    // create an array of per-size noob-stackptrs
+    auto& context = module.getContext();
+    auto noobStackPtrArrayType = llvm::ArrayType::get(llvm::Type::getInt8PtrTy(context), 64);
+    auto noobStackPtrArray = new llvm::GlobalVariable(
+        module, 
+        noobStackPtrArrayType,
+        false, 
+        llvm::GlobalVariable::PrivateLinkage, 
+        llvm::ConstantAggregateZero::get(noobStackPtrArrayType), 
+        "noob.stackptrarray"
+    );
+
+    // find all unsafe stack objects & move them
+    auto& stackSafetyAnalysis = MAM.getResult<llvm::StackSafetyGlobalAnalysis>(module);
+    for (auto& func : module) {
+        if (func.isDeclaration())
+            continue;
+
+        llvm::DenseMap<llvm::TypeSize::ScalarTy, llvm::SmallVector<llvm::AllocaInst*>> sizeToUnsafeAllocas;
+        for (auto& inst : llvm::instructions(func)) {
+            if (auto alloca = llvm::dyn_cast<llvm::AllocaInst>(&inst)) {
+                if (!stackSafetyAnalysis.isSafe(*alloca)) {
+                    auto sizeInBitsOpt = alloca->getAllocationSizeInBits(module.getDataLayout());
+                    ASSERT_ELSE_UNKOWN(sizeInBitsOpt.hasValue(), alloca); // will fail for VLAs
+                    auto sizeInBits = sizeInBitsOpt->getFixedSize(); // may fail for VLAs still
+                    auto sizeInBytes = __builtin_align_up(sizeInBits, 8) / 8;
+                    auto alignedSizeInBytes = std::bit_ceil(sizeInBytes);
+                    ASSERT_ELSE_UNKOWN(alignedSizeInBytes >= alloca->getAlign().value(), alloca); // otherwise choose the max of both
+                    sizeToUnsafeAllocas[alignedSizeInBytes].push_back(alloca);
+                }
+            }
+        }
+
+        auto insertBefore = &*func.getEntryBlock().getFirstInsertionPt();
+        llvm::DenseMap<llvm::AllocaInst*, llvm::Value*> allocaToNewStackAlloc;
+        for (auto& [size, allocas] : sizeToUnsafeAllocas) {
+            // retrieve the "thread-local" stack pointer for this size class
+            auto addressOfSizeStackPtr = llvm::ConstantExpr::getInBoundsGetElementPtr(llvm::Type::getInt8PtrTy(context), noobStackPtrArray, llvm::ConstantInt::getIntegerValue(llvm::Type::getInt64Ty(context), llvm::APInt{64, size}));
+            auto sizeStackPtr = new llvm::LoadInst(llvm::Type::getInt8PtrTy(context), addressOfSizeStackPtr, llvm::formatv("noob.stackptr.{0}", size), insertBefore);
+
+            // now allocate the necessary amount for this function and update the stackPtr slot in memory
+            auto numObjects = allocas.size();
+            auto endOfSizeStackPtr = llvm::GetElementPtrInst::CreateInBounds(llvm::Type::getInt8Ty(context), sizeStackPtr, {llvm::Constant::getIntegerValue(llvm::Type::getInt64Ty(context), llvm::APInt{64, numObjects * size})}, "", insertBefore);
+            new llvm::StoreInst(endOfSizeStackPtr, addressOfSizeStackPtr, insertBefore);
+
+            // now create the offset pointers that represent the individual allocas
+            for (uint idx = 0; idx < allocas.size(); idx++) {
+                auto alloca = allocas[idx];
+                auto offsetStackPtr = llvm::GetElementPtrInst::CreateInBounds(llvm::Type::getInt8Ty(context), sizeStackPtr, {llvm::Constant::getIntegerValue(llvm::Type::getInt64Ty(context), llvm::APInt{64, idx * size})}, "", alloca);
+                // i needn't concern myself with marking these as "noalias" here -- we should only do this at the very end of the optimization pipeline
+                // any optimizations that benefit from the aliasing information should have been done by now
+                auto castedPtr = llvm::CastInst::CreatePointerCast(offsetStackPtr, alloca->getType(), alloca->getName(), alloca);
+                alloca->replaceAllUsesWith(castedPtr);
+                alloca->eraseFromParent();
+                allocas[idx] = nullptr; // just to make sure we dont access this again
+            }
+
+            // now insert the deallocation routine at all `return` locations of this function
+            for (auto& bb : func) {
+                if (auto ret = llvm::dyn_cast<llvm::ReturnInst>(bb.getTerminator())) {
+                    // for now, we're lazy and use the old stackPtr, as a kind of "frame pointer"
+                    // we might potentially get better results by loading back from memory & recomputing here
+                    new llvm::StoreInst(sizeStackPtr, addressOfSizeStackPtr, ret);
+                }
+            }
         }
     }
 
