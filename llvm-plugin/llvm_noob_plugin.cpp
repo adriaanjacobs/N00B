8f02c622a79b7d798d4e4496c89e68da3637a1e1
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2024-09-26 17:35:49 +0200

    add llvm plugin boilerplate to load at LTO

---
+++ llvm-plugin/llvm_noob_plugin.cpp
@@ -0,0 +1,20 @@
+#include "llvm_noob.h"
+
+#include <llvm-utils/safetyanalysis/safetyanalysis.h>
+
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Passes/PassBuilder.h>
+
+// This part is the new way of registering your pass
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo() {
+    return {
+        LLVM_PLUGIN_API_VERSION, "NOOBPlugin", LLVM_VERSION_STRING,
+        [](llvm::PassBuilder &PB) {
+            PB.registerFullLinkTimeOptimizationLastEPCallback([](llvm::ModulePassManager &MPM, llvm::OptimizationLevel Level) {
+                llvm::outs() << "Registering NOOB pass for LateLTO!\n";
+                IsInBoundsAnalysis::addPassesAround<NOOBInstrumentationPass>(MPM);
+            });
+            PB.registerAnalysisRegistrationCallback(MemAccessInstrumentator::registerAnalyses);
+        }
+    };
+}
