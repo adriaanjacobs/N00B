f658c173bc260bf498a014bb0f7867d30b33aee3
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2024-10-18 16:05:36 +0200

    permit dereference checking without pointer tagging

--- include/NOOB/config.h
+++ include/NOOB/config.h
@@ -3,7 +3,7 @@
 #define TAG_WIDTH                   (8)
 
 #define TAG_POINTERS                1
-#define CHECK_POINTER_DEREFERENCES  (1 && TAG_POINTERS)
+#define CHECK_POINTER_DEREFERENCES  1
 #define CHECK_POINTER_ARITHMETIC    1
 #define REPLACE_STACK_ALLOCS        1
 #define REMAP_GLOBAS                1
