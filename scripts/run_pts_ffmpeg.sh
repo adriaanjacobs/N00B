493f413de254d08a259571f76a55c1502961b75b
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2026-02-25 18:09:33 +0000

    add baseline run to ffmpeg PTS run script

--- scripts/run_pts_ffmpeg.sh
+++ scripts/run_pts_ffmpeg.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 rm /tmp/*.mkv
-env PTS_CONCURRENT_TEST_RUNS=1 NUMBER_OF_PROCESSORS=1 ./phoronix-test-suite batch-run local/ffmpeg-baseline
-
-rm /tmp/*.mkv
 env PTS_CONCURRENT_TEST_RUNS=1 NUMBER_OF_PROCESSORS=1 ./phoronix-test-suite batch-run local/ffmpeg-noob-full
 
 rm /tmp/*.mkv
 env PTS_CONCURRENT_TEST_RUNS=1 NUMBER_OF_PROCESSORS=1 ./phoronix-test-suite batch-run local/ffmpeg-noob-libavformat
+
+rm /tmp/*.mkv
+env PTS_CONCURRENT_TEST_RUNS=1 NUMBER_OF_PROCESSORS=1 ./phoronix-test-suite batch-run local/ffmpeg-baseline
